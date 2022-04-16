27736a4eb6ddbf5aa5d59e21c432505b208be607
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-16 10:05:05 -0500

    Initial commit with starter code, and some psuedo code added by me in server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// Create the routes in this file to connect:
+// app.get for a route to the index.html
+// app.get for a route to the notes.html
+
+// app.post and/or app.use to handle a user's input
+// user's input must add the note data to the pre-existing JSON file (db.json)
