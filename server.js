9d81ea070f482211bb429b83cdeab90bd508a0f5
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-04-21 13:53:31 -0500

    set up initial server functionality using Express

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-// Anticipated dependencies:
-// Express.js
-// Node
+const express = require('express');
+
+// set up server port to run the server
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// determine the path for the routes
+const apiRoutes = require('');
+const htmlRoutes = require('');
+
 
 // On launch of this server file, the html containing the intro page will  open on default browser
 
-// 
+app.use
+app.use
+
+
+app.listen(PORT, () => {
+    console.log(``)
+})
 
